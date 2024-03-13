2b0d17c3a35dba91a906d881e46e36b06ea698bd
wbrunovieira <wbrunovieira77@gmail.com>
2024-03-13 09:57:35 -0300

    create a file declaracao

--- src-tauri/src/file.rs
+++ src-tauri/src/file.rs
@@ -1,13 +1,34 @@
-use std::env;
+use std::{env, fs::{self, File}, path::Path};
 
 pub fn get_path_from_user( ) -> Option<String> {
   if let Some(path_home) = env::var_os("HOME") {
-    Some(path_home.into_string().unwrap())
+    let path = Path::new(&path_home).join("IR-CONFERIR");
+    if !path.exists() {
+     
+      match fs::create_dir_all(&path) {
+          Ok(_) => println!("Diretório criado: {:?}", path),
+          Err(e) => println!("Erro ao criar diretório: {}", e),
+      }
+  }
+    Some(path.to_str().unwrap().to_string())
   } else {
     None
   }
 }
 
-pub fn create_file(path: &str){
-  println!("Creating file at: {}", path)
+pub fn create_file(path: &str, file_name: &str) {
+  println!("Creating file at: {}", path);
+  println!("File Name: {}", file_name);
+
+  let full_path = Path::new(path).join(file_name);
+  let full_path_str = full_path.to_str().unwrap_or("Invalid path");
+
+ match File::create(&full_path) {
+    Ok(_) => { 
+      println!("File created successfully at {}", full_path_str)
+    },
+    Err(e) => {
+      println!("Error creating file at {}: {}", full_path_str, e)
+      }
+}
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -14,7 +14,7 @@ fn greet(name: &str) -> String {
 fn main() {
     println!("Hello from Rust!");
     let path = get_path_from_user().unwrap();
-    create_file(&path);
+    create_file(&path,&"declaracao.txt");
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
