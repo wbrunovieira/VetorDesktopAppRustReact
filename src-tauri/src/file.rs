1f5a34f54c4fc9291d29c031b4a197f7b70ef969
wbrunovieira <wbrunovieira77@gmail.com>
2024-03-13 10:21:29 -0300

    read the file txt and all dec files

--- src-tauri/src/file.rs
+++ src-tauri/src/file.rs
@@ -1,4 +1,5 @@
-use std::{env, fs::{self, File}, path::Path};
+use std::{env, fs::{self, File}, io::Read, path::Path};
+use std::io::Write;
 
 pub fn get_path_from_user( ) -> Option<String> {
   if let Some(path_home) = env::var_os("HOME") {
@@ -24,11 +25,56 @@ pub fn create_file(path: &str, file_name: &str) {
   let full_path_str = full_path.to_str().unwrap_or("Invalid path");
 
  match File::create(&full_path) {
-    Ok(_) => { 
+    Ok(mut file) => { 
+      file.write_all(b"Hello, world!").unwrap();
       println!("File created successfully at {}", full_path_str)
     },
     Err(e) => {
       println!("Error creating file at {}: {}", full_path_str, e)
       }
 }
+}
+
+pub fn read_file(full_path: &str) {
+  println!("Reading file at: {}", full_path);
+
+
+  match File::open(&full_path) {
+    Ok(mut file) => { 
+      
+      let mut content = String::new();
+      file.read_to_string(&mut content).unwrap();
+      println!("File opened successfully at {}", content)
+    },
+    Err(e) => {
+      println!("Error opening file at {}:",  e)
+      }
+}
+}
+
+pub fn read_files_dec(diretorio: &str) {
+  let path = Path::new(diretorio);
+
+  if path.exists() && path.is_dir() {
+      match fs::read_dir(path) {
+          Ok(entries) => {
+              for entry in entries.filter_map(Result::ok) {
+                  let path = entry.path();
+                  if path.is_file() && path.extension().and_then(|s| s.to_str()) == Some("DEC") {
+                      match fs::read_to_string(&path) {
+                          Ok(content) => {
+                              println!("Conteúdo do arquivo {:?}:\n{}", path.file_name().unwrap(), content);
+                          }
+                          Err(e) => {
+                              println!("Erro ao ler o arquivo {:?}: {}", path.file_name().unwrap(), e);
+                          }
+                      }
+                  }
+              }
+          },
+          Err(e) => println!("Erro ao ler o diretório: {}", e),
+      }
+  } else {
+      println!("O diretório fornecido não existe ou não é um diretório.");
+  }
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,6 +5,8 @@
 mod file;
 use file::{create_file, get_path_from_user};
 
+use crate::file::{read_file, read_files_dec};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -15,6 +17,8 @@ fn main() {
     println!("Hello from Rust!");
     let path = get_path_from_user().unwrap();
     create_file(&path,&"declaracao.txt");
+    read_file("/Users/walterbrunopradovieira/IR-CONFERIR/declaracao.txt");
+    read_files_dec("/Users/walterbrunopradovieira/IR-CONFERIR");
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
