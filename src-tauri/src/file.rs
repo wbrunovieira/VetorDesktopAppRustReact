a6ce04e2ef84de50dc18593460e572bb91730e1e
wbrunovieira <wbrunovieira77@gmail.com>
2024-03-19 10:54:46 -0300

    added dados to db sqlite

--- src-tauri/src/file.rs
+++ src-tauri/src/file.rs
@@ -1,5 +1,7 @@
 use std::{env, fs::{self, File}, io::Read, path::Path};
 use std::io::Write;
+use std::fmt;
+use rusqlite::{Connection, Result};
 
 #[derive(Debug)]
 pub struct DadosDec {
@@ -13,6 +15,15 @@ pub struct DadosDec {
     doacoes_politicas: String,
     pagamentos_doacoes_outros: String,
 }
+
+
+impl fmt::Display for DadosDec {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+      write!(f, "CPF: {}, Nome: {}, Exercício: {}, Rendimentos Tributáveis: {}, Rendimentos Isentos: {}, Rendimentos Sujeitos à Tributação Exclusiva: {}, Pagamento Anual Total de Juros: {}, Doações a Partidos Políticos: {}, Pagamentos/Doações/Outros: {}",
+          self.cpf, self.nome, self.exercicio, self.rend_tributaveis, self.rend_isentos, self.rend_exclusivos, self.juros, self.doacoes_politicas, self.pagamentos_doacoes_outros)
+  }
+}
+
 
 
 pub fn get_path_from_user( ) -> Option<String> {
@@ -67,45 +78,7 @@ pub fn read_file(full_path: &str) {
 }
 
 
-// pub fn read_files_dec(diretorio: &str) {
-//   let path = Path::new(diretorio);
 
-//   if path.exists() && path.is_dir() {
-//       match fs::read_dir(path) {
-//           Ok(entries) => {
-//               for entry in entries.filter_map(Result::ok) {
-//                   let path = entry.path();
-//                   if path.is_file() && path.extension().and_then(|s| s.to_str()) == Some("DEC") {
-//                       match fs::read_to_string(&path) {
-//                           Ok(content) => {
-//                               let dados = DadosDec {
-//                                   cpf: content[21..32].to_string(),
-//                                   nome: content[39..99].trim().to_string(),
-//                                   exercicio: content[8..12].to_string(),
-//                                   rend_tributaveis: content[695..708].to_string(),
-//                                   rend_isentos: content[736..749].to_string(),
-//                                   rend_exclusivos: content[749..762].to_string(),
-//                                   juros: content[193..206].to_string(),
-//                                   doacoes_politicas: content[482..495].to_string(),
-//                                   pagamentos_doacoes_outros: content[762..775].to_string(),
-//                               };
-                              
-//                               // Aqui, você pode fazer o que precisar com os dados, como imprimir ou armazenar em algum lugar
-//                               println!("{:?}", dados);
-//                           },
-//                           Err(e) => {
-//                               println!("Erro ao ler o arquivo {:?}: {}", path.file_name().unwrap(), e);
-//                           }
-//                       }
-//                   }
-//               }
-//           },
-//           Err(e) => println!("Erro ao ler o diretório: {}", e),
-//       }
-//   } else {
-//       println!("O diretório fornecido não existe ou não é um diretório.");
-//   }
-// }
 pub fn read_files_dec(diretorio: &str) -> Vec<DadosDec> {
   let path = Path::new(diretorio);
   let mut dados_dec = Vec::new();
@@ -146,4 +119,39 @@ pub fn read_files_dec(diretorio: &str) -> Vec<DadosDec> {
 
   dados_dec
 }
+
+pub fn create_table() -> Result<()> {
+  let conn = Connection::open("dados_dec.db")?;
+
+  conn.execute(
+      "CREATE TABLE IF NOT EXISTS dados_dec (
+          id INTEGER PRIMARY KEY,
+          cpf TEXT NOT NULL,
+          nome TEXT NOT NULL,
+          exercicio TEXT NOT NULL,
+          rend_tributaveis TEXT NOT NULL,
+          rend_isentos TEXT NOT NULL,
+          rend_exclusivos TEXT NOT NULL,
+          juros TEXT NOT NULL,
+          doacoes_politicas TEXT NOT NULL,
+          pagamentos_doacoes_outros TEXT NOT NULL
+       )",
+      [],
+  )?;
+  Ok(())
+}
+
+pub fn insert_dados_dec(conn: &Connection, dados: &DadosDec) -> Result<()> {
+  conn.execute(
+      "INSERT INTO dados_dec (cpf, nome, exercicio, rend_tributaveis, rend_isentos, rend_exclusivos, juros, doacoes_politicas, pagamentos_doacoes_outros)
+       VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9)",
+      &[
+          &dados.cpf, &dados.nome, &dados.exercicio, &dados.rend_tributaveis,
+          &dados.rend_isentos, &dados.rend_exclusivos, &dados.juros,
+          &dados.doacoes_politicas, &dados.pagamentos_doacoes_outros,
+      ],
+  )?;
+  Ok(())
+}
+
 
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,9 +3,10 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 mod file;
-use file::{create_file, get_path_from_user};
+use file::{create_file, get_path_from_user,insert_dados_dec,create_table};
 
 use crate::file::{read_file, read_files_dec};
+use rusqlite::{Connection, Result};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -13,16 +14,34 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-fn main() {
-    println!("Hello from Rust!");
+fn main() -> Result<()> {
+    let conn = Connection::open("dados_dec.db")?;
+    create_table()?;
+   
     let path = get_path_from_user().unwrap();
     create_file(&path,&"declaracao.txt");
     read_file("/Users/walterbrunopradovieira/IR-CONFERIR/declaracao.txt");
     let dados_dec = read_files_dec("/Users/walterbrunopradovieira/IR-CONFERIR");
+
+
     println!("{:#?}", dados_dec);
+
+    for dado in &dados_dec {
+        println!("{}", dado);
+    }
+
+    let dados_dec = read_files_dec("/Users/walterbrunopradovieira/IR-CONFERIR");
+    for dado in dados_dec {
+        insert_dados_dec(&conn, &dado)?;
+    }
+
+  
+    
 
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+
+        Ok(())
 }
